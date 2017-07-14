95b4677742be73224b4b6dae0b51be546948dc93
german-e-mas <german.e.mas@gmail.com>
2017-07-14 07:59:27 -0300

    Router Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
-@Component() {
+@Component({
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
     `
-}
+})
 
 export class AppComponent {
   title = 'Tour of Heroes';
